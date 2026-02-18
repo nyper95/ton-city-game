243c94a8719abe27634fbdfe9bb832d09902f192
nyper95 <andyzandy9990@gmail.com>
2026-02-18 12:19:04 -0500

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 
 // ==========================================
 // CONFIGURACIÓN SUPABASE
